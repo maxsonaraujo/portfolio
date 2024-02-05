28f5b0b1b869a9b8df711b8b5d5fd3ab9d9c3c7c
Maxson Araújo <maxson.jordan@gmail.com>
2024-02-05 03:24:53 -0300

    remove unused layout imports

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,11 +1,5 @@
 import "@/app/globals.css"
-import { Inter as FontSans } from "next/font/google"
 
-import { cn } from "@/lib/utils"
-import { ThemeProvider } from "@/components/theme-provider";
-import Head from "next/head";
-import { getDictionaries } from "@/lib/dictionary";
-import { PortfolioHead } from "@/components/atoms/portifolioHead";
 export default function RootLayout({ children }: Readonly<{
   children: React.ReactNode;
 }>) {
